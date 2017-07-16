c00d931ec1502054ced552ac865428cfae5dcf71
Stephan Gudd <stephan.gudd@hs-furtwangen.de>
2017-07-16 20:56:57 +0200

    Queue und Util hinzugefügt

---
+++ src/main/java/de/gudd/Util.java
@@ -0,0 +1,9 @@
+package de.gudd;
+
+public class Util {
+	public static boolean istErstesHalbjahr(int monat){
+		if ((monat < 1) || (monat > 12)) throw new IllegalArgumentException();
+		if(monat <= 7) return true;
+		return false;
+	}
+}
